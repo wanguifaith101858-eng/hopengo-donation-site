b94a52d2ec76d7459d14cfd44965d258c17d7289
wanguifaith101858-eng <wanguifaith101858@gmail.com>
2026-07-10 09:50:46 +0300

    Your update message here

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,44 @@
 $(function () {
+  function closeMobileMenu($header) {
+    const $menu = $header.find('.mobile-nav');
+    $menu.addClass('hidden').removeClass('open');
+    $header.find('.mobile-menu-button').attr('aria-expanded', 'false');
+  }
+
   $('.mobile-menu-button').on('click', function () {
     const $button = $(this);
-    const $menu = $button.closest('header').find('.mobile-nav');
+    const $header = $button.closest('header');
+    const $menu = $header.find('.mobile-nav');
     const isOpen = $button.attr('aria-expanded') === 'true';
     $button.attr('aria-expanded', String(!isOpen));
-    $menu.toggleClass('hidden');
+    if (isOpen) {
+      $menu.addClass('hidden').removeClass('open');
+    } else {
+      $menu.removeClass('hidden').addClass('open');
+    }
   });
 
   $('.mobile-nav a').on('click', function () {
-    const $header = $(this).closest('header');
-    $header.find('.mobile-nav').addClass('hidden');
-    $header.find('.mobile-menu-button').attr('aria-expanded', 'false');
+    closeMobileMenu($(this).closest('header'));
+  });
+
+  $(document).on('keydown', function (event) {
+    if (event.key === 'Escape') {
+      $('header').each(function () {
+        const $header = $(this);
+        if (!$header.find('.mobile-nav').hasClass('hidden')) {
+          closeMobileMenu($header);
+        }
+      });
+    }
+  });
+
+  $(document).on('click touchstart', function (event) {
+    const $target = $(event.target);
+    const $header = $('header');
+    if ($header.find('.mobile-nav').not('.hidden').length && !$target.closest('header').length) {
+      closeMobileMenu($header);
+    }
   });
 
   $(window).on('resize', function () {
